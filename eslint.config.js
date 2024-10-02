13bb3857c3b1e1c79525299788b146acbfc87dab
Olowosuyi Temitope <Temz@ETZ-TOLO-AUD.etranzact.com>
2024-10-02 05:39:50 +0100

    project configuration set up

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default [
     {
         settings: {
             react: {
-                version: 'detect', // Automatically detects the React version
+                version: 'detect',
             },
         },
     },
